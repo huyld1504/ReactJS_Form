3e49ff98c24a62038bef18c69b27721609d41c27
ledinhhuy <ledinhhuy0105@gmail.com>
2023-05-29 22:58:43 +0700

    create react app

---
+++ form_react_component/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import {Router,routes} from 'react-router'
+function App() {
+  return (
+    <Router>
+      
+    </Router>
+  );
+}
+
+export default App;
---
+++ form_react_component/src/Form/FormManagement.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class FormManagement extends Component {
+  render() {
+    return (
+      <div>FormManagement</div>
+    )
+  }
+}
